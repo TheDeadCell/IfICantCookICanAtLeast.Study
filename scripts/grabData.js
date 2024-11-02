67479252ca3d5e87a5ee71fa1c214e9683105bc0
rsmiv <rsmz6c@umsystem.edu>
2024-11-02 16:58:15 -0500

    .....

--- scripts/grabData.js
+++ scripts/grabData.js
@@ -12,16 +12,19 @@ function searchMeal(searchTerm) {
     .catch(error => console.log('ERROR'))
 }
 
-export function randomMeal() {
+export function randomMeal(x, y) {
     const apiUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
     fetch(apiUrl)
     .then(res => {
         if (!res.ok) {
-            console.log(`HTTP error! Status: ${res.status}`)
+            console.log(`HTTP error! Status: ${res.status}`);
         }
-        return(res.json())
-
+        return(res.json());
     })
-    .then(data => console.log(data))
-    .catch(error => console.log('ERROR'))
+    .then(data => {
+        console.log(data);
+        x.innerText = data.meals[0].strMeal;
+        y.innerText = data.meals[0].strInstructions;
+    })
+    .catch(error => console.log('ERROR'));
 }
--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,7 @@ import {randomMeal} from "./grabData.js";
 document.getElementById("randomButton").addEventListener("click", randomRecipe); 
 
 function randomRecipe() {
-    console.log(randomMeal());
+    let nameSection = document.getElementById("recipeName");
+    let instructionsSection = document.getElementById("recipeInstructions");
+    randomMeal(nameSection, instructionsSection);
 }
