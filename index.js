1969feb1bf9f9313fe82bd82abae0f1762fc7177
Arthur Schieszer <67496505+TheDeadCell@users.noreply.github.com>
2024-11-02 15:06:09 -0500

    fixing small bugs within index.js

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 function searchMeal(searchTerm) {
-    const apiUrl = "www.themealdb.com/api/json/v1/1/search.php?s=";
+    const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
     fetch(apiUrl + searchTerm)
     .then(res => {
         if (!res.ok) {
-            console.log('HTTP error! Status: ${response.status}')
+            console.log(`HTTP error! Status: ${res.status}`)
         }
-    })
-    .then(res => {
-        return res.json()
-    })
-    .then(res => console.log(res.json()))
-    .then(data => console.log(data))
-    .catch(error => console.log('ERROR'))
-}
+        console.log(res.json())
 
-function randomMeal() {
-    const apiUrl = "www.themealdb.com/api/json/v1/1/random.php";
-    fetch(apiUrl)
-    .then(res => {
-        if (!res.ok) {
-            console.log('HTTP error! Status: ${response.status}')
-        }
     })
-    .then(res => {
-        return res.json()
-    })
-    .then(res => console.log(res.json()))
     .then(data => console.log(data))
     .catch(error => console.log('ERROR'))
 }
