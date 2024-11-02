c0fd1dc8c401a9ae9e07db3bdc67effc3360120c
Arthur Schieszer <67496505+TheDeadCell@users.noreply.github.com>
2024-11-02 15:07:37 -0500

    re-added randomMeal()

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function searchMeal(searchTerm) {
     .then(data => console.log(data))
     .catch(error => console.log('ERROR'))
 }
+
+function randomMeal(searchTerm) {
+    const apiUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+    fetch(apiUrl)
+    .then(res => {
+        if (!res.ok) {
+            console.log(`HTTP error! Status: ${res.status}`)
+        }
+        console.log(res.json())
+
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
+}
